bdb41556afc03ba3660a141052da484e703644d3
blain <blain@argalladas.eu>
2022-05-20 23:46:25 +0200

    Add new params in button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,10 +2,10 @@ import PropTypes from "prop-types"
 import "./index.scss"
 import React from "react";
 
-function Button ({ href, onClick, withstyles = true, text, icon }) {
+function Button ({ href, click, withstyles = true, text, icon, disabled=false }) {
   const Component = href ? 'a' : 'button'
   return (
-    <Component href={href} onClick={() => onClick && onClick()} className={!withstyles && 'btn-no-style'} to="/">
+    <Component href={href} onClick={() => click && click()} className={!withstyles ? 'btn-no-style' : ''} to="/" disabled={disabled}>
       {icon && <i className={icon}></i>}
       {text}
     </Component>
@@ -13,11 +13,12 @@ function Button ({ href, onClick, withstyles = true, text, icon }) {
 }
 
 Button.propTypes = {
-  onClick: PropTypes.string,
+  click: PropTypes.func,
   href: PropTypes.string,
   withstyles: PropTypes.bool,
   text: PropTypes.string.isRequired,
-  icon: PropTypes.string
+  icon: PropTypes.string,
+  disabled: PropTypes.bool
 };
 
 export default Button
